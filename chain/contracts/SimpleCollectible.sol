8b8956115aff06d1768c417ecc9518d2e946e354
Ebuka <ebuka2264@yahoo.com>
2023-08-16 15:59:49 +0100

    seperate contracts

--- chain/contracts/SimpleCollectible.sol
+++ chain/contracts/SimpleCollectible.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
 error moreFunds();
 error UriNotMapped();
