7cab8a85a8330e5917348dfecb44aeb70aaac2b5
Hussein Mubarak <husseinmubarak500@gmail.com>
2023-08-21 19:11:58 +0100

    Worked on empty guide page

--- src/app/guide/page.tsx
+++ src/app/guide/page.tsx
@@ -1,3 +1,13 @@
+import React from 'react';
+import TopNavigation from '@/common/navs/top/TopNavigation';
+
 const Guide = () => {
-  return <div>Page coming soon</div>;
+  return (
+    <>
+      <TopNavigation />
+      <h2 className='text-center'>Page coming soon</h2>
+    </>
+  );
 };
+
+export default Guide;
