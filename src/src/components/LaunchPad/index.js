b11c4d2530d0a5bc1d168b34defcc51585ca0008
Alabi Anuoluwpo <119438433+pillars001@users.noreply.github.com>
2023-08-22 11:08:51 +0100

    Add files via upload

---
+++ src/src/components/LaunchPad/index.js
@@ -0,0 +1,10 @@
+import './index.scss';
+const launchPad = () => {
+  return (
+    <div className='coming'>
+      <h1> Coming Soon!!!</h1>
+    </div>
+  );
+};
+
+export default launchPad;
---
+++ src/src/components/Profile/index.js
@@ -0,0 +1,8 @@
+import './index.scss';
+const Profile = () => (
+  <div className='coming'>
+    <h1>IT IS COMING!</h1>
+  </div>
+);
+
+export default Profile;
